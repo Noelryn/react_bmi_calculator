e2f2c6470b988a6f1246d1f5ed24e677624ee46e
Noel Ryan <noelgerard.ryan@gmail.com>
2019-03-04 15:52:44 +0100

    Creating the BMICalculator.js and Adding in functionality, modifing the display results .js to call on the bmi calculator function

--- src/Components/displayResults.js
+++ src/Components/displayResults.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import { bmiCalculation } from '../Modules/BMICalculator';
 
 class DisplayResult extends Component {
+    calculate() {
+        var weight= this.props.weight;
+        var height= this.props.height;
+
+        return bmiCalculation(weight, height);
+    }
     render() {
         return(
             <div id='response'>
+                {this.calculate()}
             </div>
         )
     }
